aee7d6daa645748d3fc94c88097fbfec0b2fcf49
nitin8440 <nitin8440@gmail.com>
2023-07-21 14:16:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("runScriptButton").addEventListener("click", function() {
+  // Your script code goes here
+  let result = "Hello, Android!"; // Replace this with your script logic
+  alert(result);
+});
